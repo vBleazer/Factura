54ce9e9006b6a5fffe56db38ebdc15f1d0dc1142
vBleazer <terrike97@hotmail.com>
2020-10-19 03:49:39 -0600

    First commit

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -73,7 +73,7 @@ class ClienteController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
         $cliente -> update($request -> all());
 
